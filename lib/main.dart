b84e18a759c0354a9b3e3ef33d96ad9abe7c9889
youssef-degachi <youssefdegachi0@gmail.com>
2024-10-20 16:55:22 +0100

    fix language part

--- lib/main.dart
+++ lib/main.dart
@@ -14,122 +14,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeProPage extends StatelessWidget {
+class HomeProPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Pro Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => QuestionPage()),
-            );
-          },
-          child: Text('Welcome'),
-        ),
-      ),
-    );
-  }
+  _HomeProPageState createState() => _HomeProPageState();
 }
 
-class QuestionPage extends StatefulWidget {
-  @override
-  _QuestionPageState createState() => _QuestionPageState();
-}
-
-class _QuestionPageState extends State<QuestionPage> {
+class _HomeProPageState extends State<HomeProPage> {
   String selectedLanguage = '';
-  String selectedCard = '';
+  int currentQuestionIndex = 0;
+  int score = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Choose the Language'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'What language do you want to use?',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  selectedLanguage = 'Arabic';
-                });
-              },
-              child: Text('Arabic'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  selectedLanguage = 'French';
-                });
-              },
-              child: Text('French'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  selectedLanguage = 'English';
-                });
-              },
-              child: Text('English'),
-            ),
-            SizedBox(height: 30),
-            if (selectedLanguage.isNotEmpty)
-              Column(
-                children: [
-                  Text(
-                    'Select the correct answer:',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => QuizPage(selectedLanguage)),
-                      );
-                    },
-                    child: Text('Start Quiz'),
-                  ),
-                ],
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class QuizPage extends StatefulWidget {
-  final String language;
-
-  QuizPage(this.language);
-
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
   List<Question> questions = [
     Question('What is 2 + 2?', ['3', '4', '5'], '4'),
     Question('What is the capital of France?', ['Berlin', 'Madrid', 'Paris'], 'Paris'),
     Question('What color do you get when you mix red and white?', ['Pink', 'Purple', 'Orange'], 'Pink'),
     // Add more questions here
   ];
-  
-  int currentQuestionIndex = 0;
-  int score = 0;
 
   void answerQuestion(String answer) {
     if (answer == questions[currentQuestionIndex].correctAnswer) {
@@ -153,20 +53,57 @@ class _QuizPageState extends State<QuizPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Quiz (${widget.language})'),
+        title: Text('Quiz App'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(questions[currentQuestionIndex].question, style: TextStyle(fontSize: 20)),
-          SizedBox(height: 20),
-          ...questions[currentQuestionIndex].answers.map((answer) {
-            return ElevatedButton(
-              onPressed: () => answerQuestion(answer),
-              child: Text(answer),
-            );
-          }).toList(),
-        ],
+      body: Center(
+        child: currentQuestionIndex < questions.length
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (selectedLanguage.isEmpty) ...[
+                    Text(
+                      'What language do you want to use?',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedLanguage = 'Arabic';
+                        });
+                      },
+                      child: Text('Arabic'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedLanguage = 'French';
+                        });
+                      },
+                      child: Text('French'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedLanguage = 'English';
+                        });
+                      },
+                      child: Text('English'),
+                    ),
+                  ] else ...[
+                    SizedBox(height: 20),
+                    Text(questions[currentQuestionIndex].question, style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 20),
+                    ...questions[currentQuestionIndex].answers.map((answer) {
+                      return ElevatedButton(
+                        onPressed: () => answerQuestion(answer),
+                        child: Text(answer),
+                      );
+                    }).toList(),
+                  ],
+                ],
+              )
+            : CircularProgressIndicator(),
       ),
     );
   }
